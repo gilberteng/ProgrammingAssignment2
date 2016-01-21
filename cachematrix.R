ba0a73f6d5c022a7ac51adb86841bddb6dac0c91
unknown <gilberteng@outlook.com>
2016-01-20 19:18:53 -0500

    Removed debug message and added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file provides two functions that stores a matrix and computes its
+## identity matrix.  If identity matrix is calculated in a previous run, it
+## will return a cached result thereby saving computation time.
 
-## Write a short comment describing this function
 ## creates a special matrix that sets and gets the value of the matrix and
 ## sets and gets the identity matrix
-
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
 	set <- function(y) {
@@ -19,16 +18,13 @@ makeCacheMatrix <- function(x = matrix()) {
 		 getsolve = getsolve)
 }
 
-## Write a short comment describing this function
 ## This calculates the identity matrix set by the makeCacheMatrix function 
 ## If the special matrix has not changed, then the cached result will return.
 ## Otherwise it will calculate the identity matrix and store it into the cache.
-
 cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
 	m <- x$getsolve()
 	if(!is.null(m)) {
-			message("getting cached data")
 			return(m)
 	}
 	data <- x$get()
